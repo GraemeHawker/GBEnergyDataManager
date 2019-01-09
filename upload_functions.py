1576e84cc07b7651b63e4569ba28d52fcfddcaf3
Graeme Hawker <graeme.hawker@strath.ac.uk>
2019-01-09 03:08:50 +0000

    Refactoring

--- upload_functions.py
+++ upload_functions.py
@@ -60,7 +60,6 @@ def message_part_to_points(raw_message_part,
                                   message_subtype,
                                   raw_message_part))
         data_set_count += 1
-    #if message_subtype == 'IMBALNGC': print(data_dict)
     return data_dict
 
 
@@ -81,15 +80,9 @@ def message_to_dict(raw_message):
             received_time: datetime
                 the timestamp of the message
             message_type: string
-                the tibco message type, one of ['BM','SYSTEM','DYNAMIC']
+                the tibco message type, e.g. one of ['BM','SYSTEM','DYNAMIC']
             message_subtype: string
-                the subtype of the message
-                for message_type == 'BM', one of
-                    ['FPN', 'QPN', 'MEL', 'MIL', 'BOD']
-                for message_type == 'SYSTEM', one of
-                    ['MID', 'FREQ']
-                for message_type == 'DYNAMIC', one of
-                    []
+                the subtype of the message, e.g. 'FPN'
             data_pairs: dict
                 time/value pairs ordered by integer key
                 each consisting of a timestamp and float value
@@ -165,7 +158,7 @@ def message_to_dict(raw_message):
                               raw_message))
     return message_dict
 
-def dict_to_sql(message_dict):
+def insert_data(message_dict):
     """
     Converts a message dictionary to SQL INSERT query strings
 
@@ -176,105 +169,34 @@ def dict_to_sql(message_dict):
 
     Returns
     -------
-    query : list
-        a list of SQL INSERT query strings
+    None
     """
-    pass
+    #TODO: check if message already processed
 
-def split_message(raw_message):
-    """
-    Takes a BM relevant message
-    and splits into a list of fields and a list of values
-
-    Parameters
-    ----------
-    raw_message : string
-        the BM data string
-
-    Returns
-    -------
-    fields: list
-        a list of strings containing field names
-
-    values: list
-        a list of strings containing field values
-
-    Raises
-    ------
-
-    """
-    message_parts = raw_message.split(',')
-    fields = []
-    values = []
-    for message in message_parts:
-        message_pair = message.split('=')
-        fields.append(message_pair[0])
-        if message_pair[0] == 'SD':
-            values.append(dtStringToDT(message_pair[1], False))
-        elif message_pair[0] in ['TS', 'TA']:
-            values.append(dtStringToDT(message_pair[1], True))
-        else:
-            values.append(message_pair[1])
-    return fields, values
-
-def dtStringToDT(raw_string, type):
-    """
-    Converts BMRA string which represents a date to a datetime object
-
-    Parameters
-    ----------
-    raw_string : string
-        the BM date/time string
-
-    type: Boolean
-        whether the string contains date and time (True) or date only (False)
-
-    Returns
-    -------
-    processed_datetime: list
-        a list of strings containing field names
-
-    Raises
-    ------
-
-    """
-    date_parts = raw_string.split(':')
-
-    if type is True:
-        processed_datetime = dt.datetime(year=int(date_parts[0]),
-                                         month=int(date_parts[1]),
-                                         day=int(date_parts[2]),
-                                         hour=int(date_parts[3]),
-                                         minute=int(date_parts[4]),
-                                         second=int(date_parts[5]))
+    if message_dict['message_type'] == 'SYSTEM':
+        insert_system_data(message_dict)
+    elif message_dict['message_type'] == 'BM':
+        insert_bm_data(message_dict)
+    elif message_dict['message_type'] == 'DYNAMIC':
+        insert_dynamic_data(message_dict)
+    elif message_dict['message_type'] == 'INFO':
+        insert_info_data(message_dict)
+    elif message_dict['message_type'] == 'BP':
+        insert_bp_data(message_dict)
     else:
-        processed_datetime = dt.datetime(year=int(date_parts[0]),
-                                         month=int(date_parts[1]),
-                                         day=int(date_parts[2]))
+        raise ValueError('Insert function not available for message type %s'
+                         % message_dict['message_type'])
 
-    return processed_datetime
+    #TODO: add message to list of processed messages
 
-
-
-def insert_bm_data(BMUID, BM_data_type, received, gmt, message, cur):
+def insert_bm_data(message_dict):
     """
-    Inserts a row of BMUID-level data
+    Generates and saves Django ORM object from message dictionary
+    in order to insert a row of BMUID-level data to DB
 
     Parameters
     ----------
-    BMUID : string
-        the ID code for the BM unit concerned
-    BM_data_type : string
-        the sub-type of BM data being processed, corresponding
-        to the SQL table to be inserted INTO
-
-        should be one of:
-            'FPN' : Final Physical Notification
-            'QPN' : Quiescent Physical Notification
-            'MEL' : Maximum Import Limit
-            'MIL' : Minimum Import Limit
-
-    received :
+    message: message dictionary
 
     Returns
     -------
@@ -284,55 +206,90 @@ def insert_bm_data(BMUID, BM_data_type, received, gmt, message, cur):
     ------
 
     """
-    if BM_data_type in ['FPN', 'QPN', 'MEL', 'MIL']:
-        #e = db.enterBMphys(subjectShort[0], subjectShort[-1], recieved, gmt, message, cur)
-        query_part_1 = "INSERT INTO tibcodata."+BM_data_type.lower()
-        query_part_2 = """
-        (bmu_id, recieved, settlement_day, settlement_period, start_time, start_level, end_time, end_level)
-        VALUES
-        (%s, %s, %s, %s, %s, %s, %s, %s)
-        """
+    #check if BMUID already in db, if not insert and log
 
-        insert_query = query_part_1 + query_part_2
+    #retrieve BMUID object from ORM
 
-        #Split message into consituent parts, extract settlement date/period
-        message_fields, message_values = split_message(message)
-        sDate = message_values[0]
-        sPeriod = message_values[1]
+    #construct associated BM object
 
-        #For each pair of values
-        for i in range(0, int(mV[2])-1):
+    #if BOAL or similar, construct associated sub objects
 
-            #Extract relevant data
-            sTime = message_values[3+(2*i)]
-            sLevel = message_values[4+(2*i)]
-            eTime = message_values[5+(2*i)]
-            eLevel = message_values[6+(2*i)]
-
-            #Pull together data for query
-            queryData = [BMUID, received, sDate, sPeriod, sTime.strftime('%H:%M:%S'), sLevel, eTime.strftime('%H:%M:%S'), eLevel]
-
-            #carry out querry
-            cur.execute(insert_query, queryData)
-
-    '''
-    elif BM_data_type in ['BOD']:
-        e = db.enterBMbod(subjectShort[0], subjectShort[-2], recieved, gmt, message, cur)
-    elif BM_data_type in ['BOALF', 'BOAL']:
-        e = db.enterBMboal(subjectShort[0], subjectShort[-1], recieved, gmt, message, cur)
-    elif BM_data_type in ['BOAV']:
-        e =db.enterBMboav(subjectShort[0], subjectShort[-2], recieved, gmt, message, cur)
-    elif BM_data_type in ['EBOCF']:
-        e =db.enterBMebocf(subjectShort[0], subjectShort[-2], recieved, gmt, message, cur)
-    '''
+    #save all as single transaction after checks
+    pass
 
 
-def insert_system_data(message):
+def insert_system_data(message_dict):
     """
+    Generates and saves Django ORM object from message dictionary
+    in order to insert a row of SYSTEM-level data to DB
+
+    Parameters
+    ----------
+    message: message dictionary
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+
     """
     pass
 
-def insert_dynamic_data(BMUID, BM_data_type, message):
+def insert_dynamic_data(message_dict):
     """
+    Generates and saves Django ORM object from message dictionary
+    in order to insert a row of BMUID-level dynamic data to DB
+
+    Parameters
+    ----------
+    message: message dictionary
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+
+    """
+    pass
+
+def insert_info_data(message_dict):
+    """
+    Generates and saves Django ORM object from message dictionary
+    in order to insert a row of info data to DB
+
+    Parameters
+    ----------
+    message: message dictionary
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+
+    """
+    pass
+
+def insert_bp_data(message_dict):
+    """
+    Generates and saves Django ORM object from message dictionary
+    in order to insert a row of BP-level data to DB
+
+    Parameters
+    ----------
+    message: message dictionary
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+
     """
     pass
