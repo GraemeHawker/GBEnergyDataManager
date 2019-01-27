e2a194b57673a2e8259f30068dac22fb9c578c76
Graeme Hawker <graeme.hawker@strath.ac.uk>
2019-01-27 23:47:55 +0000

    Added BM model definitions and manual download commands

---
+++ BMRA/management/commands/bulk_download.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+command to download and process BMRA data for specified date rahge
+"""
+import datetime as dt
+from django.core.management.base import BaseCommand, CommandError
+from ._download_functions import process_bmra_file
+
+class Command(BaseCommand):
+    help = 'downloads BMRA data for specific date range, expected 2 arguments of form yyyy-m-d'
+
+    def add_arguments(self, parser):
+        parser.add_argument('date', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        self.stdout.write('Started: {:%Y-%m-%d %H:%M:%S}'.format(dt.datetime.now()))
+        self.stdout.write("downloading data for %s" % options['date'][0])
+        start_date = dt.datetime(*[int(x) for x in options['date'][0].split('-')[:3]])
+        end_date = dt.datetime(*[int(x) for x in options['date'][1].split('-')[:3]])
+        date = start_date
+        while date <= end_date:
+            self.stdout.write('{:%Y-%m-%d %H:%M:%S}'.format(dt.datetime.now()))
+            self.stdout.write("downloading data for %s" % options['date'][0])
+            process_bmra_file(date)
+            date += dt.timedelta(days=1)
+        self.stdout.write('Finished: {:%Y-%m-%d %H:%M:%S}'.format(dt.datetime.now()))
