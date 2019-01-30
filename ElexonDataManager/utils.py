19ea65cac077951d5a174c2865c974ff46bf379a
Graeme Hawker <grgraeme.hawker@strath.ac.uk>
2019-01-30 18:01:59 +0000

    updated tests

--- ElexonDataManager/utils.py
+++ ElexonDataManager/utils.py
@@ -3,6 +3,25 @@
 common helper functions
 """
 import datetime as dt
+from django.utils import timezone
+import pytz
+
+def is_dst(datetime):
+    """
+    determines if a (timezone-aware) datetime object is within UK daylight savings
+
+    parameters
+    ----------
+    datetime : datetime object
+        timezone-aware datetime
+
+    Returns
+    -------
+    is_dst : boolean
+        True if datetime in UK daylight savings, False if not
+    """
+    return datetime.astimezone(pytz.timezone('Europe/London')).dst() > dt.timedelta(0)
+
 
 def sp_to_dt(SD, SP, period_start=True):
     """
@@ -24,17 +43,34 @@ def sp_to_dt(SD, SP, period_start=True):
     datetime : datetime.datetime
         a timezone-aware datetime object
     """
-    if period_start:
-        return dt.datetime(SD.year, SD.month, SD.day,
-                           SP//2,
-                           (SP%2)*30)
-    return dt.datetime(SD.year, SD.month, SD.day,
-                       SP//2,
-                       (SP%2)*30)+dt.timedelta(mins=30)
+
+    #minimum SP value check
+    if SP<1:
+        raise ValueError('SP value of %d less than minimum value of 1' % SP)
+
+    #maximum SP value check, taking into account transition days
+    transition_days = [dt.date(x.year, x.month, x.day) for x in pytz.timezone('Europe/London')._utc_transition_times]
+    if SD in transition_days[::2] and SP>46:    #clocks go forward
+        raise ValueError('SP value of %d exceeds maximum value of 46 for forward clock change date' % SP)
+    elif SD in transition_days[1::2] and SP>50: #clocks go back
+        raise ValueError('SP value of %d exceeds maximum value of 50 for backward clock change date' % SP)
+    elif SP>48:
+        raise ValueError('SP value of %d exceeds maximum value of 48 for non-clock change date' % SP)
+
+    #SD and SP are in BST, so calculate datetime for that timezone
+    datetime = dt.datetime(SD.year, SD.month, SD.day, tzinfo=pytz.timezone('Europe/London'))
+    datetime += dt.timedelta(minutes=(SP-1)*30)
+    print(datetime)
+    #if period end, add half an hour
+    if not period_start:
+        datetime+=dt.timedelta(minutes=30)
+
+    #convert to UTC datetime
+    return datetime.astimezone(pytz.utc)
 
 def dt_to_sp(datetime, period_start=True):
     """
-    Converts a timezone-aware datetime object to a settlement date and
+    Converts a timezone-aware datetime object to a settlement date (BST) and
     settlement period
 
     Parameters
