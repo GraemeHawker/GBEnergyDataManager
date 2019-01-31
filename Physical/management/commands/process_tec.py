b1419d35cb52f79b065d08c87fde9ced62d9f93a
Graeme Hawker <grgraeme.hawker@strath.ac.uk>
2019-01-31 18:48:57 +0000

    Updated physical models

--- Physical/management/commands/process_tec.py
+++ Physical/management/commands/process_tec.py
@@ -23,9 +23,71 @@ class Command(BaseCommand):
                                      skiprows=[0])
         self.stdout.write(str(raw_tec_data.columns))
         for index, row in raw_tec_data.iterrows():
+            # if listed owner does not exist, create it
             try:
                 owner = PowerStationOwner.objects.get(name=row['Customer Name'])
             except PowerStationOwner.DoesNotExist:
                 owner = PowerStationOwner(name=row['Customer Name'])
                 owner.save()
+
+            # if listed Connection Site does not exist, create it
+            try:
+                connection = ConnectionSite.objects.get(name=row['ConnectionSite'])
+            except PowerStationOwner.DoesNotExist:
+                connection = ConnectionSite(name=row['ConnectionSite'])
+                connection.save()
+
+            #if listed TO Zone does not exist, create it
+            try:
+                to_zone = TOZone.objects.get(name=row['HOST TO'])
+            except PowerStationOwner.DoesNotExist:
+                to_zone = TOZone(name=row['HOST TO'])
+                to_zone.save()
+
+            # if listed plant type does not exist, create it
+            try:
+                station_type = StationType.objects.get(name=row['Plant Type'])
+            except PlantType.DoesNotExist:
+                station_type = StationType(name=row['Plant Type'])
+                station_type.save()
+
+            # if power station entry does not exist, create it and bind to zone objects
+            try:
+                power_station = PowerStation.objects.get(name=row['Project Name'])
+            except PlantType.DoesNotExist:
+                power_station = PowerStation(name = row['Project Name'],
+                                             station_type = station_type,
+                                             to_zone = to_zone,
+                                             OC2_zone = None,
+                                             connection_site = connection_site
+                                             )
+                power_station.save()
+
+            # check if current owner known, if not create new relationship
+            # TODO: add date handling
+            # TODO: edge case of site being taken back by a previous owner
+            try:
+                power_station_ownership = PowerStationOwnership.objects.get(power_station = power_station,
+                                                                            power_station_owner = owner)
+            except PlantType.DoesNotExist:
+                power_station_ownership = PowerStationOwnership(power_station = power_station,
+                                                                power_station_owner = owner,
+                                                                start_date = None,
+                                                                )
+                power_station_ownership.save()
+
+            # update / create power station status
+            try:
+                power_station_status = PowerStationStatus.objects.get(power_station = power_station,
+                                                                      effective_date = row['MW Effective Date']
+                                                                      rating = row['MW Total']
+                                                                      project_status = project_status
+                                                                      )
+            except PowerStationStatus.DoesNotExist:
+                power_station_status = PowerStationStatus(power_station = power_station,
+                                                          effective_date = row['MW Effective Date']
+                                                          rating = row['MW Connected']
+                                                          project_status = project_status
+                                                          )
+                power_station_status.save()
             self.stdout.write(str(row))
