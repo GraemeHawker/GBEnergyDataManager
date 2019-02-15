2b3dc10152ac3a153dc2b5bdfa098852b984bdc3
Graeme Hawker <graeme.hawker@strath.ac.uk>
2019-02-15 02:55:35 +0000

    more date function updates

--- BMRA/management/commands/_aggregate_functions.py
+++ BMRA/management/commands/_aggregate_functions.py
@@ -13,14 +13,17 @@ def get_BMU_timeseries(bmu_id, SD_start, SD_end):
     #get raw list of FPN objects
     bmu = BMU.objects.get(id=bmu_id)
     fpns = pd.DataFrame.from_records(FPN.objects.filter(bmu=bmu,
-                                                        SD=SD_start).values())
+                                                        SD__gte=SD_start,
+                                                        SD__lte=SD_end).values())
 
-    df_fpn = pd.DataFrame(columns=['SD','SP','TS'])
+    df_fpn = pd.DataFrame(columns=['SD', 'SP', 'TS', 'VP'])
 
+    df_fpn.set_index(['SD', 'SP', 'TS'])
+    
     fpn_levels = FPNlevel.objects.filter(
         fpn__bmu=bmu,
-        fpn__SD=SD_start,
-        fpn__SD__lte=SD_end).order_by('TS','fpn__SD','fpn__SP')
+        fpn__SD__gte=SD_start,
+        fpn__SD__lte=SD_end).order_by('TS', 'fpn__SD', 'fpn__SP')
 
     for fpn_level in fpn_levels:
         df_fpn = df_fpn.append([{'TS':fpn_level.TS,
