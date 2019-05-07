d52e1819c267f74ec0e1273e3ed2546733e83edc
Graeme Hawker <graeme.hawker@strath.ac.uk>
2019-05-07 15:10:58 +0100

    added P114 upload funcs

--- P114/management/commands/_download_functions.py
+++ P114/management/commands/_download_functions.py
@@ -2,9 +2,7 @@
 Helper functions for managing P114 file downloads
 
 """
-import datetime as dt
 import urllib.request
-import gzip
 import os.path
 import json
 import requests
@@ -30,9 +28,9 @@ def get_p114_filenames_for_date(p114_date):
 
     """
     response = requests.get(P114_LIST_URL.format(ELEXON_KEY,
-                                                 P114_date.year,
-                                                 P114_date.month,
-                                                 P114_date.day))
+                                                 p114_date.year,
+                                                 p114_date.month,
+                                                 p114_date.day))
     json_data = json.loads(response.text)
     return json_data.items()
 
