9c557ab157a66a2b5842c431ef6086319210063f
Graeme Hawker <graeme.hawker@strath.ac.uk>
2023-11-22 15:56:42 +0000

    removed deprecated pandas methods

--- BMRA/management/commands/update_BMUs.py
+++ BMRA/management/commands/update_BMUs.py
@@ -15,15 +15,18 @@ class Command(BaseCommand):
         files = ['Generators.csv', 'GSP units.csv', 'Interconnectors.csv', 'Unknown.csv']
         metadata = pd.DataFrame(columns=['Type', 'BMU', 'Name', 'GSP region'])
         for file in BMU_METADATA_FILES:
-            metadata = metadata.append(pd.read_csv(BMU_METADATA_PATH + file))
+            metadata = pd.concat([metadata, pd.read_csv(BMU_METADATA_PATH + file)])
 
         for i, bmu_data in metadata.iterrows():
             #print(bmu_data)
             #station_type = StationType.objects.get(id=type_mapping[bmu_data.Type])
-            station_type = StationType.objects.get_or_create(id=type_mapping[bmu_data.Type],
+            station_type, created = StationType.objects.get_or_create(id=type_mapping[bmu_data.Type],
                                                              defaults={'name': type_mapping[bmu_data.Type],
                                                                        'supertype': type_mapping[bmu_data.Type]})
-            gsp_group = GSP_group.objects.get(id=gsp_mapping[bmu_data['GSP region']])
+            #print(gsp_mapping[bmu_data['GSP region']])
+            gsp_group, created = GSP_group.objects.get_or_create(id=gsp_mapping[bmu_data['GSP region']],
+                                                        defaults={'name': bmu_data['GSP region']})
+            #print(gsp_group)
             obj, created = BMU.objects.update_or_create(id=bmu_data.BMU,
                                                         defaults={'name': bmu_data.Name,
                                                                   'gsp_group': gsp_group,
